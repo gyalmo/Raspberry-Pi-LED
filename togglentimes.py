0a7822c225c78285c7df3f33359b06e7e60c9689
Shruti Pandey <48848864+gyalmo@users.noreply.github.com>
2020-07-21 19:28:18 +0530

    Create  togglentimes.py

---
+++ togglentimes.py
@@ -0,0 +1,12 @@
+#n=10
+
+import RPi.GPIO as GPIO
+import time
+GPIO.setmode (GPIO.BCM)
+GPIO.setwarnings (False)
+GPIO.setup (18,GPIO.HIGH)
+for i in range (0,10):
+   GPIO.output(18,GPIO.HIGH)
+   time.sleep(3)
+   GPIO.output(18,GPIO.LOW)
+   time.sleep(3)
