439401eb8f708af5296094f12ff23a215dca3587
Shruti Pandey <48848864+gyalmo@users.noreply.github.com>
2020-07-22 17:39:55 +0530

    Update Toggle n LEDs.py

--- Toggle n LEDs.py
+++ Toggle n LEDs.py
@@ -9,7 +9,6 @@ GPIO.setup (18,GPIO.OUT)
 GPIO.setup (17,GPIO.OUT)
 GPIO.setup (16,GPIO.OUT)
 for i in range (0,10):
-   
    GPIO.output(18,GPIO.HIGH)
    GPIO.output(17,GPIO.LOW)
    GPIO.output(16,GPIO.HIGH)
