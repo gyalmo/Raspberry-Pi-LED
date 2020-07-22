05eccb9662e0fa1a7af16169415f51ceb34e2905
Shruti Pandey <48848864+gyalmo@users.noreply.github.com>
2020-07-22 17:36:53 +0530

    Update Toggle n LEDs.py

--- Toggle n LEDs.py
+++ Toggle n LEDs.py
@@ -1,4 +1,11 @@
 #WAP to toggle n LEDs
 #n=3
 
+import RPi.GPIO as GPIO
+import time
+GPIO.setmode(GPIO.BCM)
+GPIO.setwarnings (False)
+GPIO.setup (18,GPIO.OUT)
+GPIO.setup (17,GPIO.OUT)
+GPIO.setup (16,GPIO.OUT)
 
