34d2599eeb2475a0524d1c76adc9c690d1775659
Shruti Pandey <48848864+gyalmo@users.noreply.github.com>
2020-07-27 19:21:35 +0530

    Update Release LED.py

--- Release LED.py
+++ Release LED.py
@@ -1,2 +1,12 @@
 #WAP to release LED
+import RPi.GPIO as GPIO
+import time
+GPIO.setmode(GPIO.BCM)
+GPIO.setwarnings(False)
+GPIO.setup(18,GPIO.OUT)
+GPIO.setup(17,GPIO.OUT)
+GPIO.setup(16,GPIO.OUT)
+GPIO.output(18,GPIO.HIGH)
+GPIO.output(17,GPIO.LOW)
+GPIO.output(16,GPIO.HIGH)
 
