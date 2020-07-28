8af054566f0839a43bca6f8f801f709525082604
Shruti Pandey <48848864+gyalmo@users.noreply.github.com>
2020-07-28 18:03:27 +0530

    Update Toggle LEDs user input.py

--- Toggle LEDs user input.py
+++ Toggle LEDs user input.py
@@ -1,2 +1,14 @@
 #WAP to while taking user input (print 1 for green , print 2 for blue , print 3 for yellow)
 
+import RPi.GPIO as GPIO
+import time
+GPIO.setmode (GPIO.BCM)
+GPIO.setwarnings (False)
+GPIO.setup (18,GPIO.OUT)
+GPIO.setup (17,GPIO.OUT)
+GPIO.setup (16,GPIO.OUT)
+GPIO.output(18,GPIO.LOW)
+GPIO.output(17,GPIO.LOW)
+GPIO.output(16,GPIO.LOW)
+y=int (input)
+If (y==1):
